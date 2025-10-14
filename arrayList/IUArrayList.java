c9535250ce96bfab21d75a72836c1836a77dad31
Jacob <jacobsmith214@u.boisestate.edu>
2025-10-14 12:04:23 -0600

    added list2 files to new arrayList Folder

---
+++ arrayList/IUArrayList.java
@@ -0,0 +1,126 @@
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class IUArrayList implements IndexedUnsortedList {
+
+    @Override
+    public void add(Object element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addAfter(Object element, Object target) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addToFront(Object element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addToRear(Object element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean contains(Object target) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object first() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object last() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int startingIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object remove(Object element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object removeFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object removeLast() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void set(int index, Object element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
