849913b9f60b67a2e4535e0fc0e3f0c0d4058e6e
Jacob <jacobsmith214@u.boisestate.edu>
2025-10-14 12:44:27 -0600

    Updated my IUArrayList.java methods

--- arrayList/IUArrayList.java
+++ arrayList/IUArrayList.java
@@ -1,126 +1,173 @@
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
-public class IUArrayList implements IndexedUnsortedList {
+/**
+ * An array-based implementation of IndexedUnsortedList
+ */
+public class IUArrayList<T> implements IndexedUnsortedList<T> {
+    private T[] array;
+    private int rear;
+    public static final int DEFAULT_CAPACITY = 10;
 
-    @Override
-    public void add(Object element) {
-        // TODO Auto-generated method stub
-        
+
+    /**
+     * initialize list with default capacity
+     */
+    public IUArrayList() {
+        // array = (T[])(new Object[DEFAULT_CAPACITY]);
+        // rear = 0;
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * initialize list with given initial capacity
+     * @param initialCapacity size of initial array
+     */
+    @SuppressWarnings(value = "unchecked")  
+    public IUArrayList(int initialCapacity) {
+       array=(T[])(new Object[initialCapacity] );
+        rear=0;
+    }
+
+    /**
+     * Double the array capacity if there is no more room to add
+     */
+    private void expandIfNecessary() {
+        if (rear == array.length) {
+           array = Arrays.copyOf(array, array.length*2);
+        }
     }
 
     @Override
-    public void add(int index, Object element) {
-        // TODO Auto-generated method stub
-        
+    public void addToFront(T element) {
+        expandIfNecessary();
+        for (int i = rear; i > 0; i--) {
+            
+        }
     }
 
     @Override
-    public void addAfter(Object element, Object target) {
-        // TODO Auto-generated method stub
-        
+    public void addToRear(T element) {
+        // Implementation here
+        // throw new UnsupportedOperationException("Unimplemented method 'addToRear'");
+        expandIfNecessary();
+        array[rear] = element;
+        rear++;
+    }   
+
+    @Override
+    public void add(T element) {
+        addToRear(element);
+    }   
+
+    @Override
+    public void addAfter(T element, T target) {
+        // Implementation here
+        throw new UnsupportedOperationException("Unimplemented method 'addAfter'");
+    }   
+
+    @Override
+    public void add(int index, T element) {
+        // Implementation here
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }   
+
+    @Override
+    public T removeFirst() {
+        // Implementation here
+        return null; // Placeholder return
     }
 
     @Override
-    public void addToFront(Object element) {
-        // TODO Auto-generated method stub
-        
+    public T removeLast() {
+        // Implementation here
+        return null; // Placeholder return
     }
 
     @Override
-    public void addToRear(Object element) {
-        // TODO Auto-generated method stub
-        
+    public T remove(T element) {        
+        // Implementation here
+        return null; // Placeholder return
     }
 
     @Override
-    public boolean contains(Object target) {
-        // TODO Auto-generated method stub
-        return false;
+    public T remove(int index) {        
+        // Implementation here
+        return null; // Placeholder return
     }
 
     @Override
-    public Object first() {
-        // TODO Auto-generated method stub
-        return null;
+    public T first() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        } 
+        return array[0];
+    }  
+
+    @Override
+    public T last() {   
+         if (isEmpty()) {
+            throw new NoSuchElementException();
+        } 
+        return array[rear - 1];
     }
 
     @Override
-    public Object get(int index) {
-        // TODO Auto-generated method stub
-        return null;
+    public T get(int index) {     
+      
+        return array[index];
+    }
+    // is the lazy or faster way to follow this practice
+
+    @Override
+    public int indexOf(T element) {    
+        // int index = -1;
+        for (int i=0;i<rear;i++){
+            // if (int i = 0; index < 0 && i < rear; i++) {
+            if (array[i].equals(element)){
+                return i;
+                // index=i;
+            }
+        }
+        // return index;
+        return -1;
     }
 
     @Override
-    public int indexOf(Object element) {
-        // TODO Auto-generated method stub
-        return 0;
+    public boolean contains(T target) {     
+        return indexOf (target) > -1;
     }
 
     @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+    public int size() {    
+        return rear;
     }
 
     @Override
-    public Iterator iterator() {
-        // TODO Auto-generated method stub
-        return null;
+    public boolean isEmpty() {     
+        return rear == 0;
     }
 
     @Override
-    public Object last() {
-        // TODO Auto-generated method stub
-        return null;
+    public void set(int index, T element) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public ListIterator listIterator() {
-        // TODO Auto-generated method stub
-        return null;
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public ListIterator listIterator(int startingIndex) {
-        // TODO Auto-generated method stub
-        return null;
+    public ListIterator<T> listIterator() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public Object remove(Object element) {
-        // TODO Auto-generated method stub
-        return null;
+    public ListIterator<T> listIterator(int startingIndex) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    @Override
-    public Object remove(int index) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Object removeFirst() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Object removeLast() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void set(int index, Object element) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public int size() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-    
 }
--- arrayList/ListTester.java
+++ arrayList/ListTester.java
@@ -224,9 +224,9 @@ public class ListTester {
 		case badList:
 			listToUse = new BadList<Integer>();
 			break;
-		// case arrayList:
-		// 	listToUse = new IUArrayList<Integer>();
-		// 	break;
+		case arrayList:
+			listToUse = new IUArrayList<Integer>();
+			break;
 //		case singleLinkedList:
 //			listToUse = new IUSingleLinkedList<Integer>();
 //			break;
